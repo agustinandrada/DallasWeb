b8750310975d4e8bd16291fd1f21ac25dae52670
Pablo Maldonado <pablomaldonado2086@gmail.com>
2023-07-27 17:51:26 -0300

    Componentización Panel de Admin

--- Client/src/Views/Admin.jsx
+++ Client/src/Views/Admin.jsx
@@ -1,57 +1,38 @@
-import axios from "axios";
 import { useState } from "react";
+import CreateProduct from "../components/CreateProduct";
+import UpdateProduct from "../components/UpdateProduct";
 
 const URL_BASE = "http://localhost:3001";
 
 function Admin() {
-  const [form, setForm] = useState({
-    name: "",
-    description: "",
-    price: "",
-  });
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    axios.post(`${URL_BASE}/post`, form);
-  };
+  const [create, setCreate] = useState(false);
+  const [update, setUpdate] = useState(false);
   return (
     <main className="bg-sky-800 w-max mx-auto rounded-md shadow-2xl mt-32 py-10">
-      <h1 className="text-center text-3xl text-white font-bold ">
-        Panel de administrador
-      </h1>
-      <form
-        className="mb-0 space-y-6 p-10 flex flex-col "
-        onSubmit={handleSubmit}
-      >
-        <input
-          onChange={handleChange}
-          className="border-2 border-gray-300 w-96 m-auto  text-sm"
-          type="text"
-          placeholder="Ingrese nombre del producto..."
-        />
-        <input
-          onChange={handleChange}
-          className="border-2 border-gray-300 w-96 m-auto text-sm"
-          type="text"
-          placeholder="Ingrese descripción del producto..."
-        />
-        <input
-          onChange={handleChange}
-          className="border-2 border-gray-300 w-96 m-auto text-sm"
-          type="text"
-          placeholder="Ingrese precio del producto..."
-        />
-        <button className="bg-slate-600 border-l-rose-950 rounded-lg text-white text-lg  mt- mx-48 p-3 hover:bg-slate-500">
-          Agregar Producto
-        </button>
-      </form>
+      {create === false && update === false ? (
+        <div>
+          <button
+            onClick={() => {
+              setCreate(true);
+            }}
+            className="border border-slate-800 bg-red-900 w-max m-5 p-1 "
+          >
+            Crear producto
+          </button>
+          <button
+            onClick={() => {
+              setUpdate(true);
+            }}
+            className="border border-slate-800 bg-red-900 w-max m-5 p-1"
+          >
+            Actualizar producto
+          </button>
+        </div>
+      ) : create === true ? (
+        <CreateProduct setCreate={setCreate} />
+      ) : (
+        <UpdateProduct />
+      )}
     </main>
   );
 }
---
+++ Client/src/components/UpdateProduct.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function UpdateProduct() {
+  //AGREGAR UN BUSCADOR POR NOMBRE PARA ENCONTRAR EL PRODUCTO MÁS RÁPIDO.
+  return (
+    <div>
+      {/* MAPEAR TODOS LOS PRODUCTOS Y AGREGARLE A CADA UNO UN ICONO PARA MODIFICAR */}
+    </div>
+  );
+}
+
+export default UpdateProduct;
