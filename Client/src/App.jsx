1cbc8b4f98f9b8cdf1e06627a176f04fca5ae97e
Agustin <agustinandrada1@gmail.com>
2023-08-17 11:24:16 -0300

    tailwind

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,9 +3,10 @@ import Home from "./Views/Home";
 import Admin from "./Views/Admin";
 import Nav from "./Views/Nav"
 
+
 function App() {
   return (
-    <div className="bg-yellow-900 bg-no-repeat bg-cover overflow-hidden">
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
       <Nav/>
       <Routes>
         <Route path="/" element={<Home />} />
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    backgroundImage: {
+      site: "url('./assets/site-bg.jpg')",
+      about: "url('./assets/about.png')",
+      services: "url('./assets/services.png')",
+    },
     extend: {},
   },
   plugins: [],
