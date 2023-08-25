d068ee26e1fdf1eaccf9f03443db7e8f77752dd2
Agustin <agustinandrada1@gmail.com>
2023-08-25 18:41:12 -0300

    asd

--- Client/src/Views/Nav.jsx
+++ Client/src/Views/Nav.jsx
@@ -6,11 +6,11 @@ function Home() {
       <div className="flex flex-row items-center justify-between text-center">
         <div className="ml-4 max-w-[150px] mt-3 invert-colors">
           <a href="/">
-          <img src={logo} alt="logo" className="w-full h-full rounded-full" />
+            <img src={logo} alt="logo" className="w-full h-full rounded-full" />
           </a>
         </div>
         <a href="https://wa.me/5493425506898" target="_blank" rel="noreferrer">
-          <button className="mr-4 text-xl text-yellow-400 font-bold font-tertiary">RESERVAS</button>
+          <button className="mr-4 text-xl text-yellow-400 font-bold font-tertiary hover:text-2xl">RESERVAS</button>
         </a>
       </div>
     </div>
